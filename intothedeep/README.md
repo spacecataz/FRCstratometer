389e6f555b97c3aab36fdb60ce7a51ea22f904fe
Dan Welling <dwelling@umich.edu>
2025-01-05 12:02:48 -0500

    Adding initial files for IntoTheDeep game

---
+++ intothedeep/README.md
@@ -0,0 +1,3 @@
+# FRC 2025 Season: Into The Deep
+
+This sub-module contains functions for the 2025 season.
---
+++ intothedeep/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+Into the Deep: 2025 FRC Season
+'''
+
+import numpy as np
+
+def get_coral():
+	pass
+
+def place_coral():
+	pass
+
+def remove_algae():
+	pass
